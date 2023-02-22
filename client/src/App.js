7c8c2733fa140170805fa01e11c4e3c6763dbd9c
Therobo77 <anshusinha8873@gmail.com>
2023-02-22 13:01:57 +0530

    structure done

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,54 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // all imports for router;
-import Password from './Components/Password'
-import Profile from './Components/Profile'
-import Recovery from './Components/Recovery'
-import Register from './Components/Register'
-import Username from './Components/Username'
-import Reset from './Components/Reset';
-import PageNotFound from './Components/PageNotFound'
-
+import Password from "./Components/Password";
+import Profile from "./Components/Profile";
+import Recovery from "./Components/Recovery";
+import Register from "./Components/Register";
+import Username from "./Components/Username";
+import Reset from "./Components/Reset";
+import PageNotFound from "./Components/PageNotFound";
 
 // Root Routes
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element:<Username></Username>
-    },
-    {
-        path: '/register',
-        element:<Register></Register>
-    },
-    {
-        path: '/Password',
-        element:<Password></Password>
-    },
-    {
-        path: '/Profile',
-        element:<Profile></Profile>
-    },
-    {
-        path: '/Recovery',
-        element:<Recovery></Recovery>
-    },
-    {
-        path: '/Reset',
-        element:<Reset></Reset>
-    },
-    {
-        path: '*',
-        element:<PageNotFound></PageNotFound>
-    },
-    
-])
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/Password",
+    element: <Password></Password>,
+  },
+  {
+    path: "/Profile",
+    element: <Profile></Profile>,
+  },
+  {
+    path: "/Recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/Reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
 
-
-const  App = () => {
+const App = () => {
   return (
     <main>
-    <RouterProvider router={router}></RouterProvider>
-     </main>
-  )
-}
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  );
+};
 
-export default  App
+export default App;
